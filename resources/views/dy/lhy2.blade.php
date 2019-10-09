510ea71effecedcff85f930c52b227faeb655c3d
ping123 <zijol.tu@pingxx.com>
2019-10-09 17:58:17 +0800

    optimize

--- resources/views/dy/lhy2.blade.php
+++ resources/views/dy/lhy2.blade.php
@@ -24,19 +24,19 @@
         }
 
         .wrap {
-            margin-top: 200px;
+            margin-top: 300px;
             perspective: 1000px; /* 视图距元素的距离 相当于摄像机 */
         }
 
         .cube {
-            width: 300px;
-            height: 300px;
+            width: 400px;
+            height: 400px;
             position: relative;
             color: #fff;
             font-size: 36px;
             font-weight: bold;
             text-align: center;
-            line-height: 300px;
+            line-height: 400px;
             transform-style: preserve-3d; /* 默认flat 2D */
             transform: rotateX(-30deg) rotateY(-70deg);
             animation: rotate 20s infinite linear; /*播放时间 播放次数为循环 缓动效果为匀速 */
@@ -58,104 +58,104 @@
         }
 
         .cube .out-back {
-            background: url("/img/WechatIMG7.jpeg");
+            background: url("/img/WechatIMG6.jpeg");
             background-size: 100% 100%;
-            transform: translateZ(-150px) rotateY(180deg);
+            transform: translateZ(-200px) rotateY(180deg);
         }
 
         .cube .out-left {
             background: url("/img/WechatIMG8.jpeg");
             background-size: 100% 100%;
-            transform: translateX(-150px) rotateY(-90deg);
+            transform: translateX(-200px) rotateY(-90deg);
         }
 
         .cube .out-right {
             background: url("/img/WechatIMG9.jpeg");
             background-size: 100% 100%;
-            transform: translateX(150px) rotateY(90deg);
+            transform: translateX(200px) rotateY(90deg);
         }
 
         .cube .out-top {
             background: url("/img/WechatIMG10.jpeg");
             background-size: 100% 100%;
-            transform: translateY(-150px) rotateX(90deg);
+            transform: translateY(-200px) rotateX(90deg);
         }
 
         .cube .out-bottom {
-            background: url("/img/WechatIMG6.jpeg");
+            background: url("/img/WechatIMG7.jpeg");
             background-size: 100% 100%;
-            transform: translateY(150px) rotateX(-90deg);
+            transform: translateY(200px) rotateX(-90deg);
         }
 
         .cube > span {
             display: block;
-            width: 150px;
-            height: 150px;
+            width: 200px;
+            height: 200px;
             border: 1px solid #fff;
             position: absolute;
             /*opacity: .7;*/
-            top: 50px;
-            left: 50px;
+            top: 75px;
+            left: 75px;
         }
 
         .cube .in-front {
             background: url("/img/WechatIMG1.jpeg");
             background-size: 100% 100%;
-            transform: translateZ(75px);
+            transform: translateZ(100px);
         }
 
         .cube .in-back {
-            background: url("/img/WechatIMG2.jpeg");
+            background: url("/img/WechatIMG1.jpeg");
             background-size: 100% 100%;
-            transform: translateZ(-75px) rotateY(180deg);
+            transform: translateZ(-100px) rotateY(180deg);
         }
 
         .cube .in-left {
             background: url("/img/WechatIMG3.jpeg");
             background-size: 100% 100%;
-            transform: translateX(-75px) rotateY(-90deg);
+            transform: translateX(-100px) rotateY(-90deg);
         }
 
         .cube .in-right {
             background: url("/img/WechatIMG4.jpeg");
             background-size: 100% 100%;
-            transform: translateX(75px) rotateY(90deg);
+            transform: translateX(100px) rotateY(90deg);
         }
 
         .cube .in-top {
             background: url("/img/WechatIMG5.jpeg");
             background-size: 100% 100%;
-            transform: translateY(-75px) rotateX(90deg);
+            transform: translateY(-100px) rotateX(90deg);
         }
 
         .cube .in-bottom {
-            background: url("/img/WechatIMG1.jpeg");
+            background: url("/img/WechatIMG2.jpeg");
             background-size: 100% 100%;
-            transform: translateY(75px) rotateX(-90deg);
+            transform: translateY(100px) rotateX(-90deg);
         }
 
         .wrap:hover .out-front {
-            transform: translateZ(250px);
+            transform: translateZ(300px);
         }
 
         .wrap:hover .out-back {
-            transform: translateZ(-250px) rotateY(180deg);
+            transform: translateZ(-300px) rotateY(180deg);
         }
 
         .wrap:hover .out-left {
-            transform: translateX(-250px) rotateY(-90deg);
+            transform: translateX(-300px) rotateY(-90deg);
         }
 
         .wrap:hover .out-right {
-            transform: translateX(250px) rotateY(90deg);
+            transform: translateX(300px) rotateY(90deg);
         }
 
         .wrap:hover .out-top {
-            transform: translateY(-250px) rotateX(90deg);
+            transform: translateY(-300px) rotateX(90deg);
         }
 
         .wrap:hover .out-bottom {
-            transform: translateY(250px) rotateX(-90deg);
+            transform: translateY(300px) rotateX(-90deg);
         }
 
     </style>
